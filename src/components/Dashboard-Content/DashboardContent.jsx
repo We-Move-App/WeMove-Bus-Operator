23a22d81ff19209ad9d25493e608be42c143dd9c
Ranjima-dev <granj@indosakura.com>
2026-05-20 11:42:52 +0530

    feat(dashboard): fetch and display wallet details in dashboard cards

--- src/components/Dashboard-Content/DashboardContent.jsx
+++ src/components/Dashboard-Content/DashboardContent.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ContentHeading from "../Reusable/Content-Heading/ContentHeading";
 import CardDetails from "./Card-Details/CardDetails";
 import CustomBarChart from "./Custom-Bar-Chart/CustomBarChart";
 import CustomPieChart from "./Custom-Pie-Chart/CustomPieChart";
 import styles from "./dashboard-content.module.css";
 import { useTranslation } from "react-i18next";
+import axiosInstance from "../../services/axiosInstance";
 
 const DashboardContent = () => {
   const { t } = useTranslation();
+  const [walletDetails, setWalletDetails] = useState(null);
+  const fetchWallet = async () => {
+    try {
+      const res = await axiosInstance.get("/wallet/details");
+      setWalletDetails(res.data.data);
+    } catch (err) {
+      console.error("Wallet fetch failed", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchWallet();
+  }, []);
 
   return (
     <>
@@ -19,7 +33,12 @@ const DashboardContent = () => {
       />
       <div className={styles.dashboardTwoHalves}>
         <div className={styles.leftContent}>
-          <CardDetails showIncomeCard={true} hideMidContent={false} />
+          <CardDetails
+            walletDetails={walletDetails}
+            showIncomeCard={true}
+            hideMidContent={false}
+          />
+          {/* <CardDetails showIncomeCard={true} hideMidContent={false} /> */}
           <CustomBarChart />
         </div>
         <div className={styles.rightContent}>
